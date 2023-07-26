7f933f56273a9b63dc8afe6c3b0b3801be9c1ff0
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-07-26 13:08:52 +0200

    add rubik font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.vue'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
